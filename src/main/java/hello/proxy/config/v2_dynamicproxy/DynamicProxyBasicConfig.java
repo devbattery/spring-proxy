19361a585fd71f4b486689441d6451683c7626fa
devbattery <devbattery@outlook.com>
2024-11-08 19:18:25 +0900

    섹션 6: 동적 프록시 기술
    JDK 동적 프록시 - 적용 2

--- src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyBasicConfig.java
+++ src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyBasicConfig.java
@@ -6,7 +6,7 @@ import hello.proxy.app.v1.OrderRepositoryV1;
 import hello.proxy.app.v1.OrderRepositoryV1Impl;
 import hello.proxy.app.v1.OrderServiceV1;
 import hello.proxy.app.v1.OrderServiceV1Impl;
-import hello.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import hello.proxy.config.v2_dynamicproxy.handler.LogTraceFilterHandler;
 import hello.proxy.trace.logtrace.LogTrace;
 import java.lang.reflect.Proxy;
 import org.springframework.context.annotation.Bean;
@@ -15,12 +15,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DynamicProxyBasicConfig {
 
+    private static final String[] PATTERNS = {"request*", "order*", "save*"};
+
     @Bean
     public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
         OrderControllerV1 orderControllerV1 = new OrderControllerV1Impl(orderServiceV1(logTrace));
         return (OrderControllerV1) Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(),
                 new Class[]{OrderControllerV1.class},
-                new LogTraceBasicHandler(orderControllerV1, logTrace));
+                new LogTraceFilterHandler(orderControllerV1, logTrace, PATTERNS));
     }
 
     @Bean
@@ -28,16 +30,16 @@ public class DynamicProxyBasicConfig {
         OrderServiceV1 orderServiceV1 = new OrderServiceV1Impl(orderRepositoryV1(logTrace));
         return (OrderServiceV1) Proxy.newProxyInstance(OrderServiceV1.class.getClassLoader(),
                 new Class[]{OrderServiceV1.class},
-                new LogTraceBasicHandler(orderServiceV1, logTrace));
+                new LogTraceFilterHandler(orderServiceV1, logTrace, PATTERNS));
     }
 
     @Bean
     public OrderRepositoryV1 orderRepositoryV1(LogTrace logTrace) {
-        OrderRepositoryV1 orderRepository = new OrderRepositoryV1Impl();
+        OrderRepositoryV1 orderRepositoryV1 = new OrderRepositoryV1Impl();
         return (OrderRepositoryV1) Proxy.newProxyInstance(
                 OrderRepositoryV1.class.getClassLoader(),
                 new Class[]{OrderRepositoryV1.class},
-                new LogTraceBasicHandler(orderRepository, logTrace));
+                new LogTraceFilterHandler(orderRepositoryV1, logTrace, PATTERNS));
     }
 
 }
